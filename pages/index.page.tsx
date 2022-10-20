2cde45bd0198626cd30d11ed6fbf03a6d7e72dc5
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2022-10-20 15:02:54 -0300

    criacao do projeto

---
+++ pages/index.page.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+	return (
+		<div >
+			<Head>
+				<title>Gol a Gol Paredão Goalkeeper Academy</title>
+				<meta name="description" content="Evento de gol a gol da maior academia de goleiros do brasil! Ta esperando o que para fazer sua inscrição goleirão?" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			teste
+		</div>
+	)
+}
+
+export default Home
