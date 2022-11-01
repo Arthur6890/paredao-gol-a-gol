03057d591e63aaa33a4b3c320213ad5263403043
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2022-11-01 00:21:50 -0300

    adicao de texto no saiba mais

--- components/knowMore/index.tsx
+++ components/knowMore/index.tsx
@@ -13,9 +13,20 @@ export function KnowMore() {
 					<h3 className={styles.text}>
 						A competição é a famosa disputa de jogos conhecidos como “Gol a Gol”, comuns nas escolas e brincadeiras entre amigos que gostam do futebol, contudo esta é adaptada para os arqueiros mostrarem do que são capazes e que também são ótimos jogadores com os pés.
 						<br />
+						<br />
 						Os participantes, além de estarem competindo, demonstram suas habilidades e treinam chutes e defesas no gol.
 						<br />
+						<br />
 						O Gol a Gol explora dois fundamentos: o “tiro de gol”, quando a bola é lançada de uma longa distância e a defesa desses chutes.
+						<br />
+						<br />
+						Para confirmar sua inscrição basta fazer o pagamento da inscrição. Ao preencher o formulário você será redirecionado para a página na qual você deverá anexar o comprovante de pagamento.
+						<br />
+						<br />
+						O valor da inscrição é R$ 0,00 e a chave para pagamento via pix é adalbertodioliveira@gmail.com
+						<br />
+						<br />
+						O evento acontecerá na Arena Comercial localizada no endereço R. Américo Magalhães, 375 - Barreiro, Belo Horizonte
 					</h3>
 					<div className={styles.buttonsDiv}>
 						<Link href={"/docs/Regulamento.pdf"}>
